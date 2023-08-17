ea4b3d05a0093b4c51eacee0e16d1ec6f9ea6dee
totjae <105477042+totjae@users.noreply.github.com>
2023-08-17 15:01:02 +0900

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,8 @@
+count=1
+
+while true; do
+        current_date=$(date +"%Y-%m-%d %H:%M:%S")
+        echo "ETH: $current_date - New job from atia.attach-hub.minerpoolhub.com:17025
+ETH: GPU0: 22.910Mh/s GPU1: 22.924Mh/s GPU2: 22.315Mh/s"
+        sleep 1
+done  
